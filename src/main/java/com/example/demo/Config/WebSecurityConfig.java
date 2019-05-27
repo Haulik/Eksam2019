8f166485a75532fb358ad94337c5423e323f87a8
ande4694 <knudsen879@hotmail.com>
2019-05-27 13:41:35 +0200

    thomse har brilieret

--- src/main/java/com/example/demo/Config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/Config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin", "/adminCreateBooking", "/adminBooking", "/AdminTest").hasRole("ADMIN")
+                .antMatchers("/admin", "/adminCreateBooking", "/adminBooking").hasRole("ADMIN")
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
